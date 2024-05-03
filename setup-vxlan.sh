c2a605526a1b35034086e419862b0f5668033dab
Maen Artimy <artimym@gmail.com>
2024-05-03 13:14:05 -0300

    add ospf

--- setup-vxlan.sh
+++ setup-vxlan.sh
@@ -16,7 +16,7 @@
 
 
 # Create VXLAN interfaces and companion bridges for VNI 100
-docker exec -it clab-simple-r1 ip link add vxlan100 type vxlan id 100 dstport 4789 local 10.0.0.5 nolearning
+docker exec -it clab-simple-r1 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.1 nolearning
 docker exec -it clab-simple-r1 brctl addbr br100
 docker exec -it clab-simple-r1 brctl addif br100 vxlan100
 docker exec -it clab-simple-r1 brctl stp br100 off
@@ -24,7 +24,7 @@ docker exec -it clab-simple-r1 ip link set up dev br100
 docker exec -it clab-simple-r1 ip link set up dev vxlan100
 docker exec -it clab-simple-r1 brctl addif br100 eth2
 
-docker exec -it clab-simple-r3 ip link add vxlan100 type vxlan id 100 dstport 4789 local 10.0.1.5 nolearning
+docker exec -it clab-simple-r3 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.3 nolearning
 docker exec -it clab-simple-r3 brctl addbr br100
 docker exec -it clab-simple-r3 brctl addif br100 vxlan100
 docker exec -it clab-simple-r3 brctl stp br100 off

33c0a54646b434df0a1b58c775ed12613d346825
Maen Artimy <artimym@gmail.com>
2024-05-04 20:52:01 +0000

    add topology

---
+++ ring/setup-vxlan-static.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Create VXLAN interfaces and companion bridges for VNI 100
+docker exec -it clab-ring-r1 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.1 nolearning
+docker exec -it clab-ring-r1 brctl addbr br100
+docker exec -it clab-ring-r1 brctl addif br100 vxlan100
+docker exec -it clab-ring-r1 brctl stp br100 off
+docker exec -it clab-ring-r1 ip link set up dev br100
+docker exec -it clab-ring-r1 ip link set up dev vxlan100
+docker exec -it clab-ring-r1 brctl addif br100 eth2
+
+docker exec -it clab-ring-r1 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.2
+docker exec -it clab-ring-r1 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.3
+docker exec -it clab-ring-r1 bridge fdb append aa:bb:05:05:05:05 dev vxlan100 dst 1.1.1.2
+docker exec -it clab-ring-r1 bridge fdb append aa:bb:06:06:06:06 dev vxlan100 dst 1.1.1.3
+
+docker exec -it clab-ring-r2 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.2 nolearning
+docker exec -it clab-ring-r2 brctl addbr br100
+docker exec -it clab-ring-r2 brctl addif br100 vxlan100
+docker exec -it clab-ring-r2 brctl stp br100 off
+docker exec -it clab-ring-r2 ip link set up dev br100
+docker exec -it clab-ring-r2 ip link set up dev vxlan100
+docker exec -it clab-ring-r2 brctl addif br100 eth2
+
+docker exec -it clab-ring-r2 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.1
+docker exec -it clab-ring-r2 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.3
+docker exec -it clab-ring-r2 bridge fdb append aa:bb:04:04:04:04 dev vxlan100 dst 1.1.1.1
+docker exec -it clab-ring-r2 bridge fdb append aa:bb:06:06:06:06 dev vxlan100 dst 1.1.1.3
+
+docker exec -it clab-ring-r3 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.3 nolearning
+docker exec -it clab-ring-r3 brctl addbr br100
+docker exec -it clab-ring-r3 brctl addif br100 vxlan100
+docker exec -it clab-ring-r3 brctl stp br100 off
+docker exec -it clab-ring-r3 ip link set up dev br100
+docker exec -it clab-ring-r3 ip link set up dev vxlan100
+docker exec -it clab-ring-r3 brctl addif br100 eth2
+
+docker exec -it clab-ring-r3 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.1
+docker exec -it clab-ring-r3 bridge fdb append 00:00:00:00:00:00 dev vxlan100 dst 1.1.1.2
+docker exec -it clab-ring-r3 bridge fdb append aa:bb:04:04:04:04 dev vxlan100 dst 1.1.1.1
+docker exec -it clab-ring-r3 bridge fdb append aa:bb:05:05:05:05 dev vxlan100 dst 1.1.1.2

9aeca9a5f788abcbf7449b1d549e7f4478471509
Maen Artimy <artimym@gmail.com>
2024-05-04 00:15:45 +0000

    cleanup

--- setup-vxlan.sh
+++ setup-vxlan.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#docker exec -it clab-simple-r1 ip link add br0 type bridge
-#docker exec -it clab-simple-r1 ip link set dev br0 up
-#docker exec -it clab-simple-r1 ip link add vxlan10 type vxlan id 10 dstport 4789
-#docker exec -it clab-simple-r1 ip link set dev vxlan10 up
-#docker exec -it clab-simple-r1 ip link set vxlan10 master br0
-#docker exec -it clab-simple-r1 ip link set eth2 master br0
-
-#docker exec -it clab-simple-r3 ip link add br0 type bridge
-#docker exec -it clab-simple-r3 ip link set dev br0 up
-#docker exec -it clab-simple-r3 ip link add vxlan10 type vxlan id 10 dstport 4789
-#docker exec -it clab-simple-r3 ip link set dev vxlan10 up
-#docker exec -it clab-simple-r3 ip link set vxlan10 master br0
-#docker exec -it clab-simple-r3 ip link set eth2 master br0
-
-
 # Create VXLAN interfaces and companion bridges for VNI 100
 docker exec -it clab-simple-r1 ip link add vxlan100 type vxlan id 100 dstport 4789 local 1.1.1.1 nolearning
 docker exec -it clab-simple-r1 brctl addbr br100
